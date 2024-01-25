0eadb4289116cb1845b56a891c5812a2f67ba651
muhyasin89 <muhyasin89@gmail.com>
2024-01-25 15:26:43 +0700

    [js/app/blog]add modal in app blog

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,11 +1,26 @@
+import Typography from "@mui/material/Typography";
 import MainLayout from "../layouts/MainLayout";
+import TableComponent from "../components/Table";
+
+import BasicModal from "../components/Modal";
 
 function Users() {
   return (
     <MainLayout>
-    <div>Users</div>
+      <BasicModal children={<>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Text in a modal
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+        </>}/>
+      
+
+      <div>Users</div>
+      <TableComponent />
     </MainLayout>
-  )
+  );
 }
 
-export default Users
+export default Users;
