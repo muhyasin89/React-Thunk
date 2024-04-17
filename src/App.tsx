aaff300d018e5ff8de9c4b22722cfc0e800bc9d5
muhyasin89 <muhyasin89@gmail.com>
2024-04-18 06:04:25 +0700

    [base]make bare minimum

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import  Dashboard  from "./pages/Dashboard";
 
 import NotFound from "./pages/NotFound"
-import Posts from "./pages/Posts/Index";
-import Users from "./pages/Users";
-import AddForm from "./pages/Posts/Add";
-import SignIn from "./pages/auth/SignIn";
-import SignUp from "./pages/auth/SignUp";
-
-
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/register" element={<SignUp />} />
-     
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/posts/add" element={<AddForm />} />
-        
-        
         <Route path="*" element={<NotFound />} />
       </Routes>
       </>
--- src/components/Navigation/Index.tsx
+++
@@ -1,9 +0,0 @@
-
-import MenuItem from "./MenuItem";
-import { menu } from "./menu";
-
-function Navigation() {
-  return menu.map((item, key) => <MenuItem key={key} item={item} />);
-}
-
-export default Navigation;
